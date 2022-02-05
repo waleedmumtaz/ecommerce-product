04041972138aa5ecceb3116ec62fbbf87b78266f
Waleed <waleedthedev@gmail.com>
2022-02-05 21:45:36 +0500

    Clean up and setup according to style guides

--- src/App.svelte
+++ src/App.svelte
@@ -1,65 +1,38 @@
 <script>
-  import logo from './assets/svelte.png'
-  import Counter from './lib/Counter.svelte'
 </script>
 
+<svelte:head>
+  <title>E-commerce Product Page</title>
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link
+    rel="preconnect"
+    href="https://fonts.gstatic.com"
+    crossorigin="anonymous"
+  />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@400;700&display=swap"
+    rel="stylesheet"
+  />
+</svelte:head>
+
 <main>
-  <img src={logo} alt="Svelte Logo" />
-  <h1>Hello world!</h1>
+  <!-- Collections
+  Men
+  Women
+  About
+  Contact
 
-  <Counter />
+  Sneaker Company
 
-  <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-    apps.
-  </p>
+  Fall Limited Edition Sneakers
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p>
+  These low-profile sneakers are your perfect casual wear companion. Featuring a 
+  durable rubber outer sole, they’ll withstand everything the weather can offer.
+
+  $125.00
+  50%
+  $250.00
+
+  0
+  Add to cart -->
 </main>
-
-<style>
-  :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  }
-
-  main {
-    text-align: center;
-    padding: 1em;
-    margin: 0 auto;
-  }
-
-  img {
-    height: 16rem;
-    width: 16rem;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4rem;
-    font-weight: 100;
-    line-height: 1.1;
-    margin: 2rem auto;
-    max-width: 14rem;
-  }
-
-  p {
-    max-width: 14rem;
-    margin: 1rem auto;
-    line-height: 1.35;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      max-width: none;
-    }
-
-    p {
-      max-width: none;
-    }
-  }
-</style>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,29 @@
 const config = {
-  content: ["./src/**/*.{html,js,svelte,ts}"],
+  content: ['./src/**/*.{html,js,svelte,ts}'],
 
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cstm: {
+          primary: {
+            orange: 'hsl(26, 100%, 55%)',
+            'pale-orange': 'hsl(25, 100%, 94%)',
+          },
+          neutral: {
+            'very-dark-blue': 'hsl(220, 13%, 13%)',
+            'dark-grayish-blue': 'hsl(219, 9%, 45%)',
+            'grayish-blue': 'hsl(220, 14%, 75%)',
+            'light-grayish-blue': 'hsl(223, 64%, 98%)',
+          },
+        },
+      },
+      fontFamily: {
+        'kumbh-sans': ['Kumbh Sans', 'sans-serif'],
+      },
+    },
   },
 
   plugins: [],
-};
+}
 
-module.exports = config;
+module.exports = config
