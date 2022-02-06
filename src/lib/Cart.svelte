117099a2f780ef86b9d2d38169e4f9ec190607ea
Waleed <waleedthedev@gmail.com>
2022-02-06 18:08:08 +0500

    Developed cart

---
+++ src/lib/Cart.svelte
@@ -0,0 +1,11 @@
+<div>
+  <div class="flex min-h-[16rem] w-full flex-col rounded-md bg-white shadow-lg">
+    <p class="p-5 font-bold">Cart</p>
+    <hr />
+    <div
+      class="text-cstm-neutral-dark-grayish-blue m-5 flex flex-1 items-center justify-center font-bold"
+    >
+      <p>Your cart is empty.</p>
+    </div>
+  </div>
+</div>
--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,49 +1,69 @@
 <script>
-  import { fly } from 'svelte/transition'
+  import { fade, fly } from 'svelte/transition'
+  import Cart from './Cart.svelte'
   import IconCart from './IconCart.svelte'
 
   let navIsOpen = false
+  let cartIsOpen = false
 
   const toggleNav = () => {
     navIsOpen = !navIsOpen
   }
+
+  const toggleCart = () => {
+    cartIsOpen = !cartIsOpen
+  }
 </script>
 
 <header>
-  <!-- Mobile nav -->
-  <nav class="relative">
-    {#if navIsOpen}
+  <div class="relative">
+    <!-- Mobile nav -->
+    <nav class="relative">
+      {#if navIsOpen}
+        <div
+          transition:fly={{ x: -50, duration: 500 }}
+          class={`${
+            navIsOpen
+              ? 'absolute left-0 top-0 min-h-screen w-2/3 bg-white p-6'
+              : 'hidden'
+          }`}
+        >
+          <button on:click|preventDefault={toggleNav}>
+            <img src="/assets/icon-close.svg" alt="close icon" class="mb-10" />
+          </button>
+          <ul class="space-y-4 text-lg font-bold">
+            <li><a href="/#">Collections</a></li>
+            <li><a href="/#">Men</a></li>
+            <li><a href="/#">Women</a></li>
+            <li><a href="/#">About</a></li>
+            <li><a href="/#">Contact</a></li>
+          </ul>
+        </div>
+      {/if}
+      <div class="flex items-center justify-between p-5">
+        <div class="flex items-center gap-4">
+          <button on:click|preventDefault={toggleNav}>
+            <img src="/assets/icon-menu.svg" alt="hamburger menu" />
+          </button>
+          <img src="/assets/logo.svg" alt="logo" />
+        </div>
+        <div class="flex items-center gap-6">
+          <button on:click={toggleCart}>
+            <IconCart />
+          </button>
+          <img src="/assets/image-avatar.png" alt="avatar" class="w-6" />
+        </div>
+      </div>
+    </nav>
+
+    <!-- Cart -->
+    {#if cartIsOpen}
       <div
-        transition:fly={{ x: -50, duration: 500 }}
-        class={`${
-          navIsOpen
-            ? 'absolute left-0 top-0 min-h-screen w-2/3 bg-white p-6'
-            : 'hidden'
-        }`}
+        transition:fade={{ duration: 250 }}
+        class={`${cartIsOpen ? 'absolute top-16 w-full p-2' : 'hidden'}`}
       >
-        <button on:click|preventDefault={toggleNav}>
-          <img src="/assets/icon-close.svg" alt="close icon" class="mb-10" />
-        </button>
-        <ul class="space-y-4 text-lg font-bold">
-          <li><a href="/#">Collections</a></li>
-          <li><a href="/#">Men</a></li>
-          <li><a href="/#">Women</a></li>
-          <li><a href="/#">About</a></li>
-          <li><a href="/#">Contact</a></li>
-        </ul>
+        <Cart />
       </div>
     {/if}
-    <div class="flex items-center justify-between p-5">
-      <div class="flex items-center gap-4">
-        <button on:click|preventDefault={toggleNav}>
-          <img src="/assets/icon-menu.svg" alt="hamburger menu" />
-        </button>
-        <img src="/assets/logo.svg" alt="logo" />
-      </div>
-      <div class="flex items-center gap-6">
-        <IconCart />
-        <img src="/assets/image-avatar.png" alt="avatar" class="w-6" />
-      </div>
-    </div>
-  </nav>
+  </div>
 </header>
